1fb21e61dd6686344bad1408a3e6f36c1549455d
Maxr1998 <max.rumpf1998@gmail.com>
2014-12-28 22:59:00 +0100

    restart app for changes to take effect

--- app/src/main/java/de/Maxr1998/xposed/maxlock/ui/SettingsActivity.java
+++ app/src/main/java/de/Maxr1998/xposed/maxlock/ui/SettingsActivity.java
@@ -1,7 +1,10 @@
 package de.Maxr1998.xposed.maxlock.ui;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -102,5 +105,19 @@ public class SettingsActivity extends ActionBarActivity implements Authenticatio
     protected void onResume() {
         super.onResume();
         StatusBarTintApi.sendColorChangeIntent(getResources().getColor(R.color.primary_red_dark), -3, getResources().getColor(R.color.black), -3, this);
+    }
+
+    public void restart() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.app_name)
+                .setMessage(R.string.restart_required)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = getIntent();
+                        finish();
+                        startActivity(intent);
+                    }
+                }).create().show();
     }
 }
