e821460da4c8deaff22cadf8477d0cd825072b2f
Maxr1998 <max.rumpf1998@gmail.com>
2015-03-13 22:07:01 +0100

    add nice animation to options item in appslist, better colors

--- app/src/main/java/de/Maxr1998/xposed/maxlock/ui/settings/appslist/AppListAdapter.java
+++ app/src/main/java/de/Maxr1998/xposed/maxlock/ui/settings/appslist/AppListAdapter.java
@@ -32,6 +32,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
 import android.widget.CheckBox;
 import android.widget.Filter;
 import android.widget.ImageButton;
@@ -209,11 +213,37 @@ public class AppListAdapter extends RecyclerView.Adapter<AppListAdapter.ViewHold
                     prefsPackages.edit()
                             .putBoolean(key, true)
                             .commit();
-                    // TO-DO: Custom reveal animations
+                    AnimationSet anim = new AnimationSet(true);
+                    anim.addAnimation(AnimationUtils.loadAnimation(mContext, R.anim.appslist_settings_rotate));
+                    anim.addAnimation(AnimationUtils.loadAnimation(mContext, R.anim.appslist_settings_translate));
+                    hld.options.startAnimation(anim);
                     hld.options.setVisibility(View.VISIBLE);
                 } else {
                     prefsPackages.edit().remove(key).commit();
-                    hld.options.setVisibility(View.GONE);
+                    AnimationSet animOut = new AnimationSet(true);
+                    animOut.addAnimation(AnimationUtils.loadAnimation(mContext, R.anim.appslist_settings_rotate_out));
+                    animOut.addAnimation(AnimationUtils.loadAnimation(mContext, R.anim.appslist_settings_translate_out));
+                    animOut.setAnimationListener(new Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(Animation animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animation animation) {
+                            animation = new TranslateAnimation(0.0f, 0.0f, 0.0f, 0.0f);
+                            animation.setDuration(1);
+                            hld.options.startAnimation(animation);
+                            hld.options.setVisibility(View.GONE);
+                            hld.options.clearAnimation();
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animation animation) {
+
+                        }
+                    });
+                    hld.options.startAnimation(animOut);
                 }
             }
         });
