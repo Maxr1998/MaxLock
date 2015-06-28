0859b96dcbaf78b0c193134cf27eef808136ea7e
Maxr1998 <max.rumpf1998@gmail.com>
2015-06-28 20:01:01 +0200

    revert

--- app/src/main/java/de/Maxr1998/xposed/maxlock/Main.java
+++ app/src/main/java/de/Maxr1998/xposed/maxlock/Main.java
@@ -40,7 +40,7 @@ public class Main implements IXposedHookZygoteInit, IXposedHookLoadPackage {
 
     @Override
     public void initZygote(StartupParam startupParam) throws Throwable {
-        XposedBridge.log("LoadedclassMainMaxLock.");
+        XposedBridge.log("Loaded class Main @ MaxLock.");
         PREFS_PACKAGES = new XSharedPreferences(MY_PACKAGE_NAME, Common.PREFS_PACKAGES);
         PREFS_ACTIVITIES = new XSharedPreferences(MY_PACKAGE_NAME, Common.PREFS_ACTIVITIES);
         /*PREFS_IMOD = new XSharedPreferences(MY_PACKAGE_NAME, Common.PREFS_IMOD);
