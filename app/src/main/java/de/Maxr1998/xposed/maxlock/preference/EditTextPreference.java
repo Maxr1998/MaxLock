c995e036697616b8fc92afda7eda0c62f64b53dc
Maxr1998 <max.rumpf1998@gmail.com>
2015-12-03 21:56:50 +0100

    fix EditTextPreference being empty

---
+++ app/src/main/java/de/Maxr1998/xposed/maxlock/preference/EditTextPreference.java
@@ -0,0 +1,51 @@
+/*
+ * MaxLock, an Xposed applock module for Android
+ * Copyright (C) 2014-2015  Maxr1998
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.Maxr1998.xposed.maxlock.preference;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import de.Maxr1998.xposed.maxlock.R;
+
+public class EditTextPreference extends android.preference.EditTextPreference {
+
+    public EditTextPreference(Context context) {
+        super(context);
+    }
+
+    public EditTextPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public EditTextPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onAddEditTextToDialogView(@NonNull View dialogView, @NonNull EditText editText) {
+        ViewGroup container = (ViewGroup) dialogView.findViewById(R.id.edittext_container);
+        if (container != null) {
+            container.addView(editText, ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+        }
+    }
+}
