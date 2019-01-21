9d482e5d49e9827453facf34aaea5e11adb7d2c6
Maxr1998 <max.rumpf1998@gmail.com>
2019-01-21 10:18:58 +0100

    Update gradle tools

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,5 +1,5 @@
 object Config {
-    const val androidGradleVersion = "3.3.0-rc03"
+    const val androidGradleVersion = "3.4.0-beta01"
     const val kotlinVersion = "1.3.11"
 
     val languages =
