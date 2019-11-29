0d6746264f0910aca432fdab99ecd8003ce755bd
Maxr1998 <max.rumpf1998@gmail.com>
2019-11-29 20:10:49 +0100

    Add missing API class

--- androidhiddenapi/src/main/java/android_hidden/app/ActivityManager.java
+++ androidhiddenapi/src/main/java/android_hidden/app/ActivityManager.java
@@ -2,6 +2,7 @@ package android_hidden.app;
 
 import android.app.IActivityManager;
 import android.content.ComponentName;
+import android.graphics.Bitmap;
 import android.graphics.Rect;
 
 public class ActivityManager {
@@ -23,5 +24,66 @@ public class ActivityManager {
         public boolean visible;
         // Index of the stack in the display's stack list, can be used for comparison of stack order
         public int position;
+    }
+
+    public static class RunningTaskInfo {
+        /**
+         * A unique identifier for this task.
+         */
+        public int id;
+
+        /**
+         * The stack that currently contains this task.
+         */
+        public int stackId;
+
+        /**
+         * The component launched as the first activity in the task.  This can
+         * be considered the "application" of this task.
+         */
+        public ComponentName baseActivity;
+
+        /**
+         * The activity component at the top of the history stack of the task.
+         * This is what the user is currently doing.
+         */
+        public ComponentName topActivity;
+
+        /**
+         * Thumbnail representation of the task's current state.  Currently
+         * always null.
+         */
+        public Bitmap thumbnail;
+
+        /**
+         * Description of the task's current state.
+         */
+        public CharSequence description;
+
+        /**
+         * Number of activities in this task.
+         */
+        public int numActivities;
+
+        /**
+         * Number of activities that are currently running (not stopped
+         * and persisted) in this task.
+         */
+        public int numRunning;
+
+        /**
+         * Last time task was run. For sorting.
+         */
+        public long lastActiveTime;
+
+        /**
+         * True if the task can go in the docked stack.
+         */
+        public boolean supportsSplitScreenMultiWindow;
+
+        /**
+         * The resize mode of the task. See ActivityInfo#resizeMode.
+         */
+        public int resizeMode;
     }
 }
