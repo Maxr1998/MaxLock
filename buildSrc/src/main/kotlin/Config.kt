a5ecfd87aab556db7d56321e80af2196cf26d339
Maxr1998 <max.rumpf1998@gmail.com>
2020-02-13 22:52:56 +0100

    Update gradle build plugin

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,5 +1,5 @@
 object Config {
-    const val androidGradleVersion = "3.6.0-beta04"
+    const val androidGradleVersion = "3.6.0-rc03"
     const val kotlinVersion = "1.3.61"
 
     val languages =
