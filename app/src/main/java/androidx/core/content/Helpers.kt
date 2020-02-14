27c1d35071a96294de2ef97e1bac7266fd04ed4e
Maxr1998 <max.rumpf1998@gmail.com>
2020-02-14 11:04:07 +0100

    Update target SDK and build tools
    - Remove AndroidX Core-KTX because the build tools suck, replace the single used function with own code.

---
+++ app/src/main/java/androidx/core/content/Helpers.kt
@@ -0,0 +1,35 @@
+/*
+ * MaxLock, an Xposed applock module for Android
+ * Copyright (C) 2014-2018  Max Rumpf alias Maxr1998
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package androidx.core.content
+
+import android.annotation.SuppressLint
+import android.content.SharedPreferences
+
+@SuppressLint("ApplySharedPref")
+inline fun SharedPreferences.edit(
+        commit: Boolean = false,
+        action: SharedPreferences.Editor.() -> Unit
+) {
+    val editor = edit()
+    action(editor)
+    if (commit) {
+        editor.commit()
+    } else {
+        editor.apply()
+    }
+}
